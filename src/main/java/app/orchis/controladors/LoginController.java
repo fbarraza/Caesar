37b9449b219ee5ec099cbf5cee45db491652a4bb
Marc <3xlneet@gmail.com>
2017-11-17 17:24:57 +0100

    Millora codi controller i plantilla JavaMail

--- src/main/java/app/orchis/controladors/LoginController.java
+++ src/main/java/app/orchis/controladors/LoginController.java
@@ -12,6 +12,7 @@ import app.orchis.utils.eines.AppPropertiesFileHelper;
 import app.orchis.utils.eines.PropertiesHelperException;
 import java.net.URL;
 import java.util.List;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,6 +21,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -45,52 +52,103 @@ public class LoginController implements Initializable{
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //emf = Persistence.createEntityManagerFactory( "app.orchis.persistencia");
-        //manager = emf.createEntityManager();
         
     }
     
-    @FXML protected void Login(ActionEvent actionEvent) throws ConfigurationException{
-        // Creem el gestor de la persistència
+    protected void bloqueig(){
+        tfUser.setEditable(false);
+        tfPasswd.setEditable(false);
+        btLogin.setDisable(true);
+        emf.close();
+    }
+    
+    protected void intents(boolean usuari, boolean password){
+        if(!usuari){
+            intents_n++;
+            if (intents_n == 3){
+                tfInfo.setText("Has fallat el teu login tres vegades! S'ha informat a l'admin i app bloquejada");
+                bloqueig();
+            }
+            else{
+                tfInfo.setText("Usuari erroni, tens "+intents_n+" intents restants.");
+            }            
+        }
         
+        else if(!password){
+            intents_n++;
+            if (intents_n == 3){
+                tfInfo.setText("Has fallat el teu login tres vegades! S'ha informat a l'admin i app bloquejada");
+                bloqueig();
+            }
+            else{
+                tfInfo.setText("Contrassenya erronia, tens "+intents_n+" intents restants.");
+            }            
+        }
+    }
+    
+    protected void enviarmissatge(){
+        // Recipient's email ID needs to be mentioned.
+        String to = "3xlneet@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "m3xlneet@gmail.com";
+
+        // Assuming you are sending email from localhost
+        String host = "localhost";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+        try {
+           // Create a default MimeMessage object.
+           MimeMessage message = new MimeMessage(session);
+
+           // Set From: header field of the header.
+           message.setFrom(new InternetAddress(from));
+
+           // Set To: header field of the header.
+           message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+           // Set Subject: header field
+           message.setSubject("This is the Subject Line!");
+
+           // Now set the actual message
+           message.setText("This is actual message");
+
+           // Send message
+           Transport.send(message);
+           System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+           mex.printStackTrace();
+        }        
+    }
+    
+    //Botó login
+    @FXML protected void Login(ActionEvent actionEvent) throws ConfigurationException{
+        //Variables per obtenir els valors i fer el login
         String username = tfUser.getText();        
-        boolean found=false;
+        boolean login=false,passwd=false;
         int i,x=0;
         
         for(i=0;i<llista.size();i++){
             if(llista.get(i).getLogin().equals(username)){
-                found = true;
-                x = i;
+                login = true;
+                if(llista.get(i).getPasswd().equals(tfPasswd.getText())){
+                    passwd=true;
+                    user = llista.get(i);
+                }
+                else{
+                    login = false;
+                }
             }
         }
-        
-        if(!found){
-            intents_n++;
-            if (intents_n == 3){
-                tfInfo.setText("Has introduit un usuari erroni tres vegades! S'ha informat a l'admin i app bloquejada");
-                tfUser.setEditable(false);
-                tfPasswd.setEditable(false);
-                btLogin.setDisable(true);
-                emf.close();
-            }
-            else{
-                tfInfo.setText("Usuari no trobat, tens "+intents_n+" intents restants.");
-            }
+        if(!login){
+            intents(login,passwd);
         }
-        
-        else{
-            user = llista.get(x);
-        }
-        /*
-        for(Usuari u : llista)
-        {
-            System.out.println(u);
-        }
-
-        if (manager.isOpen()) {
-            manager.close();
-        }
-        emf.close();   */     
-    }
-    
+    }   
 }
